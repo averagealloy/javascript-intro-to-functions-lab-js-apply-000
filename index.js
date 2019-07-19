c6f5d5ca74d15fe93ca99e34aee3439a4212643f
Mike McTighe <m.mctighe10@gmail.com>
2019-07-19 19:31:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ if (string.toLowerCase() === string) {
 } else if (string.toUpperCase() === string){
   return "YES INDEED!"
 }else {
-  return 'I love you, too.' 
+  return 'I love you, too.'
 }
 }
